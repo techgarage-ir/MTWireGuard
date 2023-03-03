5e3cb89ba4b0742663ec70a1881221c4352f4035
Kazem ma79 <kazem2000_k@yahoo.com>
2023-03-03 23:42:01 +0330

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,8 +4,8 @@ dotnet publish -c Release -o published
 echo Publish done!
 echo.
 echo Build docker image...
-docker buildx build --no-cache --platform linux/amd64 -t mtwg .
+docker buildx build --no-cache --platform linux/amd64 -t mtwireguard .
 echo Docker image built!
 echo Save output file!
-docker save mtwg > mtwg.tar
+docker save mtwireguard > mtwg.tar
 echo Finish
